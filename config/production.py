f6ba87daddb1f94c4d4c232734a7dc5a1a8b744d
김재성 <zhqmfkv@naver.com>
2021-12-04 18:38:00 +0900

    production 오타 수정

--- config/production.py
+++ config/production.py
@@ -1,6 +1,6 @@
 from config.default import *
 
-SQLALCHEMY_DATABSE_URI = 'sqlite:///{}'.format(os.path.join(BASE_DIR, 'pybo.db'))
+SQLALCHEMY_DATABASE_URI = 'sqlite:///{}'.format(os.path.join(BASE_DIR, 'pybo.db'))
 SQLALCHEMY_TRACK_MODIFICATIONS = False
 SECRET_KEY = b'7\xdd\xdb\xf3W\x8b/8\xafh\x00/*\xca\x8f7'
 
