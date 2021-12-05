56d91330dbb0e5074f4da3cc9eaeb903a3600704
김재성 <zhqmfkv@naver.com>
2021-12-05 21:36:48 +0900

    404 오류 등록 및 migrate오류 수정

--- pybo/__init__.py
+++ pybo/__init__.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template
 from flask_migrate import Migrate
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import MetaData
@@ -6,6 +6,8 @@ from sqlalchemy import MetaData
 from flaskext.markdown import Markdown
 
 
+def page_not_found(e):
+    return render_template('404.html'), 404
 
 
 naming_convention = {
@@ -24,7 +26,7 @@ def create_app():
     app = Flask(__name__)
     app.config.from_envvar('APP_CONFIG_FILE')
 
-    #ORM
+    # ORM
     db.init_app(app)
     if app.config['SQLALCHEMY_DATABASE_URI'].startswith('sqlite'):
         migrate.init_app(app, db, render_as_batch=True)
@@ -33,7 +35,7 @@ def create_app():
 
     from . import models
 
-    #블루 프린트
+    # 블루 프린트
     from .views import main_views, question_views, answer_views, auth_views, comment_views, vote_views
     app.register_blueprint(main_views.bp)
     app.register_blueprint(question_views.bp)
@@ -42,13 +44,15 @@ def create_app():
     app.register_blueprint(comment_views.bp)
     app.register_blueprint(vote_views.bp)
 
-    #필터
+    # 필터
     from .filter import format_datetime
     app.jinja_env.filters['datetime'] = format_datetime
 
-    #markdown
+    # markdown
     # nl2br >> 줄바꿈을 br로 변경, fenced_code : 코드표시기능
     Markdown(app, extensions=['nl2br', 'fenced_code'])
 
+    # 오류 페이지
+    app.register_error_handler(404, page_not_found)
 
     return app
